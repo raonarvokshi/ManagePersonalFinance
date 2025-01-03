8631f0351e603edc3a4e2e48166bdb53312cd901
raonarvokshi <raonar.vokshi@gmail.com>
2025-01-03 14:55:21 +0100

    commit

--- frontend/main.py
+++ frontend/main.py
@@ -12,28 +12,10 @@ import base64
 
 BASE_URL = "http://127.0.0.1:8000"
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-logo_path = os.path.join(current_dir, "logo.png")
-
-
-def get_image_as_base64(image_path):
-    with open(image_path, "rb") as img_file:
-        return base64.b64encode(img_file.read()).decode("utf-8")
-
 
 def main_app():
     st.title("Personal Finance Manager 💰")
     st.markdown("---")
-
-    if os.path.exists(logo_path):
-        logo_base64 = get_image_as_base64(logo_path)
-        logo_html = f"""
-        <div style="display: flex; justify-content: center; align-items: center; height: 100px;">
-            <img src="data:image/png;base64,{logo_base64}" style="max-width: 100%; height: auto; width: 300px;">
-        </div>
-        """
-        with st.sidebar:
-            st.markdown(logo_html, unsafe_allow_html=True)
 
     menu = ["Home", "Add Income", "Update Income", "Delete Income", "Add Expense",
             "Update Expense", "Delete Expense", "Financial Report", "Logout"]
